c9fe30f83eddc157fcc091d4b2fc02113a6d1fac
ahd odeh <ahedoudeh21@gmail.com>
2025-06-24 11:57:59 +0300

    change db

--- app/database.py
+++ app/database.py
@@ -1,19 +1,22 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from urllib.parse import quote_plus
-import os
 from dotenv import load_dotenv
 
 load_dotenv()
 
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_NAME = os.getenv("DB_NAME")
-DB_HOST = os.getenv("DB_HOST")
 
-DB_PASS_ENCODED = quote_plus(DB_PASS)
+DATABASE_URL = os.getenv("DATABASE_URL")
 
-DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS_ENCODED}@{DB_HOST}/{DB_NAME}"
+if not DATABASE_URL:
+   
+    DB_USER = os.getenv("DB_USER")
+    DB_PASS = os.getenv("DB_PASS")
+    DB_NAME = os.getenv("DB_NAME")
+    DB_HOST = os.getenv("DB_HOST")
+    DB_PASS_ENCODED = quote_plus(DB_PASS)
+    DATABASE_URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASS_ENCODED}@{DB_HOST}/{DB_NAME}"
 
 engine = create_engine(DATABASE_URL)
 
