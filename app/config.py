8114d274ae27d901c875e4c925394307500062d6
ahd odeh <ahedoudeh21@gmail.com>
2025-08-01 12:54:17 +0300

    cors and images updated

--- app/config.py
+++ app/config.py
@@ -1,5 +1,5 @@
 
-# BASE_URL = "https://bayyinti-project.onrender.com"
-BASE_URL = "http://127.0.0.1:8000"
+BASE_URL = "https://bayyinti-project.onrender.com"
+# BASE_URL = "http://127.0.0.1:8000"
 
 MEDIA_PATH = "/uploads/"
--- app/main.py
+++ app/main.py
@@ -1,27 +1,36 @@
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles 
-import os
+from fastapi.staticfiles import StaticFiles
+from fastapi.middleware.cors import CORSMiddleware 
 
+import os
 from app.database import Base, engine
 from app import models
-from app.routers import room_router, property_listing_router, image_router, shared_space_router, student_router
+from app.routers import room_router, property_listing_router, image_router, shared_space_router, student_router, user_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"], 
+    allow_credentials=True,
+    allow_methods=["*"], 
+    allow_headers=["*"],  
+)
 
 app.mount("/static", StaticFiles(directory=os.path.join(os.getcwd(), "static")), name="static")
 app.mount("/uploads", StaticFiles(directory=os.path.join(os.getcwd(), "uploads")), name="uploads")
 
-Base.metadata.create_all(bind=engine)  
+Base.metadata.create_all(bind=engine)
 
 app.include_router(room_router)
 app.include_router(property_listing_router)
 app.include_router(image_router)
 app.include_router(shared_space_router)
 app.include_router(student_router)
+app.include_router(user_router)
 
 @app.get("/")
 def read_root():
     return {"message": "API is running"}
 
 # uvicorn app.main:app --reload
-#
